d11f0143876ec1d5625ba06941e5652c5c7d82b8
varvara-sergeeva <raunuperetemheru@gmail.com>
2022-09-01 23:48:17 +0300

    scroll animation on timeline

--- source/js/timeline.js
+++ source/js/timeline.js
@@ -1,20 +1,46 @@
-import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+const animItems = document.querySelectorAll(".anim-items");
 
-const scrollTracker = document.querySelector(".timeline__line");
-const scrollTrackingTimeline = new ScrollTimeline({
-  source: document.scrollingElement,
-  orientation: "block",
-  scrollOffsets: [CSS.persent(0), CSS.persent(100)],
-});
+if (animItems.length > 0) {
+  //событие при которым происходит всё описанное ниже
+  window.addEventListener('scroll', animOnScroll);
 
-//scrollTracker.animate(
-//  {
-//    transform: ["scaleX(0)", "scaleX(1)"]
-//  },
-//  {
-//    duration: 1,
-//    timeline: scrollTrackingTimeline,
-//  }
-//);
+  function animOnScroll(params) {
+    // создаём цикл
+    for (let index = 0; index < animItems.length; index++) {
+      // собираем все элемента для анимации и сохраняем в массив
+      const animItem = animItems[index];
+      // заводим переменнуюБ которая получит высоту нашего объекта
+      const animItemHeight = animItem.offsetHeight;
+      // переменная ниже определяетнасколько далеко верх объекта находится от топа страницы
+      const animItemOffset = offset(animItem).top;
+      // задаем коэфициент, который регулирует старт анимации 1/4 высоты элемента
+      const animStart = 4;
 
+      // задаём функцию по точке запуска при скролле
+      // вначале из высоты окна браузере вычетаем высоту нахождения элемента от топа поделённую на коэф
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
+      //может не понадобится - это если высота эл-та выше окна браузера (спозиционированна)
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      //теперь добавляем класс при определенных условиях
+      // если проскролленых пикселей больше чем позиция объекта минус точка старта ИИ прокрутили меньше чем позиция объекта плюс его высота(тип в пространстве объекта еще) то здесь добавляем класс
+      if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('active');
+      } else {
+        animItem.classList.remove('active');
+      }
+
+    }
+
+    //функция определяющая удалённость от верха
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {top: rect.top + scrollTop, left:rect.left + scrollLeft}
+    }
+  }
+}
